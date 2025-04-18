8acc319491643e2174a3ccf9be9fccd86095bf74
Muhammad Asif <m.asif@corp.payactiv.com>
2025-04-18 15:55:06 +0500

    first commit

---
+++ TodoApp.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace TodoApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // // Arrange
+            // var todoService = new TodoService();
+            // var todoItem = new TodoItem { Title = "Sample Todo" };
+
+            // // Act
+            // todoService.AddTodo(todoItem);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
